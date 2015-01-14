c16a146d5cc02a37de831694a14b301ed032b9ad
sonia12 <ali.caceres.so@gmail.com>
2015-01-14 15:25:01 -0400

    lo que yo hice

---
+++ sonia/formulario.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="utf-8">  
+	<title> FORMULARIO </title>
+</head>
+<body>
+
+   <div id="page">
+   	<header>
+   		<div id="logo">
+   			<img src="img/imagen_for.jpg" width="" height="">
+   		</div>
+   		<div id="TITULO">
+   			<h1>FORMULARIO</h1>
+   		</div>
+   		<div id="MENU">
+   		  <ul>
+   		  	<li><a href="#">INICIO</a></li>
+   		  	<li><a href="http://www.w3schools.com/html/html_form_input_types.asp"></a>HTML</li>
+   		  	<li><a href="https://plus.google.com/u/0/">GMAIL</a></li>
+   		  </ul>
+   		 </div>
+
+   	</header>
+    <div id="sideBar"> 
+      <article>
+      <h4> CONOCIENDONOS UN POCO</h4>
+      <p>
+      es simplemente el texto
+       de relleno de las imprentas 
+       y archivos de texto. 
+       Lorem Ipsum ha sido el texto de 
+       relleno estándar de las industrias 
+       desde el año 1500, cuando un impresor
+        (N. del T. persona que se dedica a la 
+        imprenta) desconocido usó una galería 
+        de textos y los mezcló de tal manera 
+        que logró hacer un libro de textos 
+        especimen. 
+        No sólo sobrevivió 500 años,
+         sino que tambien ingresó como texto de 
+         relleno en documentos electrónicos, 
+         quedando esencialmente igual al original. 
+         Fue popularizado en los 60s con 
+         la creación de las hojas "Letraset", 
+         las cuales contenian pasajes de Lorem
+          Ipsum, y más recientemente con software 
+          de autoedición,
+           como por ejemplo Aldus PageMaker, 
+           el cual incluye versiones de Lorem Ipsum.
+           </p>
+           </article>
+    </div>
+    <div id="contenido">
+    	<h4>DATOS PERSONALES</h4>
+    	<form action="action_page.php">
+    		nombre <br>
+    		<input type="text" name="nombre" value="sonia">
+             <br> 
+             apellido <br>
+    		<input type="text" name="apellido" value="caceres">
+    		<br>
+    		contraseña <br>
+    		<input type="password" name="pws">
+    		<br>
+    		<input type="submit" value="aceptar">
+    	</form>
+
+
+    </div>
+   </div>
+
+</body>
+</html>
